b4851c95671d35bbde608d5b35c879530c7fc061
Cody Deckard <cjdeckard@gmail.com>
2017-05-04 19:22:55 +0000

    Adds Bootstrap and Updates Gulp Scripts
    Adds bootstrap to application and updates the gulp scripts for
    application deployment. Utilizes gulp-filter and overrides main
    bootstrap files so that CSS is moved with copyVendor script.
    Index.pug was updated to include links to bootstrap. External JS
    libraries are also concatenated into a single vendor.js file.

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,14 @@ let pug = require('gulp-pug');
 let bowerFiles = require('gulp-main-bower-files');
 let uglify = require('gulp-uglify');
 let concat = require('gulp-concat');
-
+let cleanCSS = require('gulp-clean-css');
+let filter = require('gulp-filter');
 
 let srcDirectory = './app/src';
 let distDirectory = './public';
 let vendorDirectory = './public/assets/vendors';
+let filterJS = filter('**/*.js', {restore: true});
+let filterCSS = filter('**/*.css', {restore: true});
 
 gulp.task('compileHTML', () => {
     gulp.src(srcDirectory+"/**/*.pug")
@@ -25,17 +28,53 @@ gulp.task('copyJS', () => {
     
 });
 
-gulp.task('copyVendorJS', () => {
+gulp.task('copyVendor', () => {
     
-    gulp.src('./bower.json').pipe(bowerFiles())
+    gulp.src('./bower.json')
+        .pipe(bowerFiles(
+            {
+                overrides: {
+                    bootstrap: {
+                        main: [
+                            './dist/js/bootstrap.js',
+                            './dist/css/*.min.*',
+                            './dist/fonts/*.*'
+                            
+                            ]
+                    }
+                }
+            }
+        
+        ))
+        .pipe(filterJS)
+        .pipe(concat('vendor.js'))
+        .pipe(filterJS.restore)
         .pipe(gulp.dest(vendorDirectory));
     
 });
 
-gulp.task('copyVendorJSMini', () => {
+gulp.task('copyVendorMini', () => {
     
-    gulp.src('./bower.json').pipe(bowerFiles())
+    gulp.src('./bower.json')
+        .pipe(bowerFiles(
+            {
+                overrides: {
+                    bootstrap: {
+                        main: [
+                            './dist/js/bootstrap.js',
+                            './dist/css/*.min.*',
+                            './dist/fonts/*.*'
+                            
+                            ]
+                    }
+                }
+            }
+        
+        ))
+        .pipe(filterJS)
         .pipe(uglify())
+        .pipe(concat('vendor.js'))
+        .pipe(filterJS.restore)
         .pipe(gulp.dest(vendorDirectory));
     
 });
@@ -49,6 +88,6 @@ gulp.task('copyJSMini', () => {
     
 });
 
-gulp.task('build', ['compileHTML', 'copyJS', 'copyVendorJS']);
+gulp.task('build', ['compileHTML', 'copyJS', 'copyVendor']);
 
-gulp.task('build:dist', ['compileHTML', 'copyJSMini', 'copyVendorJSMini']);
+gulp.task('build:dist', ['compileHTML', 'copyJSMini', 'copyVendorMini']);
