747ec88922ad1fd6ca12e761362eec8053d31bc9
Cody Deckard <cjdeckard@gmail.com>
2017-09-27 22:31:06 +0000

    Adds Credit to Yelp for Their API
    Adds credit to Yelp for using their API.

--- app/src/pages/MainPage.js
+++ app/src/pages/MainPage.js
@@ -11,32 +11,20 @@ import ResultsComponent from '../components/ResultsComponent'
 })
 export default class MainPage extends Component {
  render() {
-     
-     let rv1 = (
-         <div className="container-fluid">
-            <div className="row">
-                <div className="col-md-4 col-md-offset-4 col-xs-offset-2 col-xs-8">
-                    <SearchComponent />
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-md-8 col-md-offset-2 col-xs-offset-2 col-xs-8">
-                    <ResultsComponent /> 
-                </div>
-            </div>
-        </div>
-         )
-         
+        
         let rv2 = (
             <div className="container-fluid">
             <div className="row">
                 <div className="col-xl-4 col-xl-offset-4 col-xs-12">
                     <SearchComponent />
                 </div>
+            <div className="row">
+               <h6 style={{textAlign: "right", marginRight: "10px"}}>Search Results Courtesy of Yelp</h6>
+            </div>
             </div>
                     <ResultsComponent /> 
         </div>    
-            
+             
         )
      
     return rv2
